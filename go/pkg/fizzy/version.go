5627b24c5c04ef73def8d9a23c6c0eb04525bb3d
Rob Zolkos <rob@zolkos.com>
2026-03-10 21:13:10 -0400

    Bump version to 0.1.2

--- go/pkg/fizzy/version.go
+++ go/pkg/fizzy/version.go
@@ -1,7 +1,7 @@
 package fizzy
 
 // Version is the current version of the Fizzy Go SDK.
-const Version = "0.1.1"
+const Version = "0.1.2"
 
 // APIVersion is the Fizzy API version this SDK targets.
 const APIVersion = "2026-03-01"
