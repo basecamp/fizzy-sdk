2643d21ecb6569b1058e8035b7c78857130bab52
Rob Zolkos <rob@zolkos.com>
2026-04-14 09:23:53 -0400

    Fix Kotlin CI target mismatch and refresh rubric audit

--- kotlin/generator/build.gradle.kts
+++ kotlin/generator/build.gradle.kts
@@ -8,10 +8,6 @@ application {
     mainClass.set("com.basecamp.fizzy.generator.MainKt")
 }
 
-tasks.withType<JavaCompile>().configureEach {
-    options.release.set(25)
-}
-
 tasks.named<JavaExec>("run") {
     workingDir = rootProject.projectDir
 }
